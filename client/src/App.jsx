37405f3d3717bd3e6367edb8eecadbd384f11141
Mxdhushri <madhushriwagh15@gmail.com>
2024-07-09 19:06:15 +0530

    grateful_first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Button}  from '@/components/ui/button' //we imported this from components
+const App = () => {
+  return (
+    <div>
+     <Button> Click me</Button>    {/*in react we write sabkuch(page ui-card,button,form) in div */}
+    </div>
+  )
+}
+
+export default App 
