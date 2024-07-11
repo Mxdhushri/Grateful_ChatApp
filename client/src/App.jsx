30347da4d1c57544b5125dcdf7bbf1cf760d15c6
Mxdhushri <madhushriwagh15@gmail.com>
2024-07-11 18:54:39 +0530

    Client side done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Routes , Route, Navigate } from 'react-router-dom' // components and package
 import {Button}  from '@/components/ui/button' //we imported this from components
+import Auth from './pages/auth'
+import Chat from './pages/chat'
+import Profile from './pages/profile'
 const App = () => {
   return (
-    <div>
-     <Button> Click me</Button>    {/*in react we write sabkuch(page ui-card,button,form) in div */}
-    </div>
+        /*in react we write sabkuch(page ui-card,button,form) in div */
+    <BrowserRouter>
+      <Routes>
+        < Route path="/auth" element={<Auth/>} />
+        < Route path="/chat" element={<Chat/>} />
+        < Route path="/profile" element={<Profile/>} />
+        < Route path="*" element={<Navigate to="/auth"/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
