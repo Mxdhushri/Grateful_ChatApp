462dd6d8f70db7110787ab3edf9a6bde6483f3eb
Madhu <madhushriwagh15@gmail.com>
2025-03-19 18:07:27 +0530

    Updated Code

--- client/src/components/Room.jsx
+++ client/src/components/Room.jsx
@@ -8,7 +8,7 @@ const Room = () => {
   const {roomId} = useParams()
 
   const myMeeting = async(element) => {
-    const appId = 451679893
+    const appId = 1904747946
     const serverSecret = import.meta.env.VITE_ZEGOCLOUD_SERVER_SECRET
     const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appId, serverSecret, roomId, Date.now().toString(), 'Madhushri')
     const zp = ZegoUIKitPrebuilt.create(kitToken)
